64c8ee5d0092a7b89ed38aa371e70ab7107e5349
Otabek-Kholmirzaev <otabekmirzo1@mail.ru>
2021-07-17 11:40:48 -0700

    'init'

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        text={showAdd ? "Close" : "Add"}
+        color={showAdd ? "red" : "green"}
+        onClick={onAdd}
+      />
+    </header>
+  );
+};
+
+// const headingStyle = {
+//   color: "red",
+//   backgroundColor: "black",
+// };
+
+// Header.defaultProps = {
+//   title: 1,
+// };
+
+// Header.propTypes = {
+//   title: PropTypes.string.isRequired,
+// };
+
+export default Header;
